ddc34e39f2f221f45ed494df7c620941f117a7c2
Chetna Ambe <chetnaambe75@gmail.com>
2025-12-21 00:10:28 +0530

    Increment Decrement Task

---
+++ First Dom TAsk/Day 13 Dom/task.js
@@ -0,0 +1,17 @@
+var inc = document.querySelector(`#inc`)
+var dec = document.querySelector(`#dec`)
+var h1 = document.querySelector(`h1`)
+
+var a = 0;
+inc.addEventListener(`click`,function(){
+a++
+h1.innerHTML = a
+h1.style.color = `green`
+})
+
+
+dec.addEventListener(`click`,function(){
+a--
+h1.innerHTML = a
+h1.style.color = `red`
+})
