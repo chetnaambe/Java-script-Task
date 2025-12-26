eade5fd9bf1147e76c8301a4178e0994f67c1971
Chetna Ambe <chetnaambe75@gmail.com>
2025-12-27 00:19:31 +0530

    Piano task Complited

---
+++ Piano task/script.js
@@ -0,0 +1,57 @@
+  var a = new Audio(`./p/a.mp3`);
+     var b = new Audio(`./p/b.mp3`);
+      var c = new Audio(`./p/c.mp3`);
+       var d = new Audio(`./p/d.mp3`);
+        var e = new Audio(`./p/e.mp3`);
+         var f = new Audio(`./p/f.mp3`);
+          var g = new Audio(`./p/g.mp3`);
+           var h = new Audio(`./p/h.mp3`);
+            var i = new Audio(`./p/i.mp3`);
+
+            var one = document.querySelector(`#one`)
+               var two = document.querySelector(`#two`)
+                  var three = document.querySelector(`#three`)
+                     var four = document.querySelector(`#four`)
+                        var five = document.querySelector(`#five`)
+                           var six= document.querySelector(`#six`)
+                            var seven= document.querySelector(`#seven`)
+                             var eight= document.querySelector(`#eight`)
+                                    var nine= document.querySelector(`#nine`)
+
+  one.addEventListener(`click`,function(){
+      a.currentTime = 0;
+ a.play();
+  })
+
+    two.addEventListener(`click`,function(){
+      b.currentTime = 0;
+ b.play();
+  })
+      three.addEventListener(`click`,function(){
+      c.currentTime = 0;
+ c.play();
+  })
+      four.addEventListener(`click`,function(){
+      d.currentTime = 0;
+ d.play();
+  })
+      five.addEventListener(`click`,function(){
+      e.currentTime = 0;
+e.play();
+  })
+      six.addEventListener(`click`,function(){
+      f.currentTime = 0;
+ f.play();
+  })
+      seven.addEventListener(`click`,function(){
+      g.currentTime = 0;
+ g.play();
+  })
+        eight.addEventListener(`click`,function(){
+      h.currentTime = 0;
+ h.play();
+  })
+        nine.addEventListener(`click`,function(){
+      i.currentTime = 0;
+ i.play();
+  })
