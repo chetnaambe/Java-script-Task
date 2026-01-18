c9db191843b59949a9e5f687bf928de78e0c2be3
Chetna Ambe <chetnaambe75@gmail.com>
2026-01-18 16:24:59 +0530

    Advance Javascript

--- Keywords Advance js/oops/script.js
+++ Keywords Advance js/oops/script.js
@@ -1,4 +1,4 @@
-// Prototype ek shared memory hai jo afford nahi kar sakta example middlile class family on ebike
+
 
 class Sketch {
     constructor(){
@@ -10,7 +10,7 @@ class Sketch {
     }
 
 }
-// isko class ke bahar isliye likh hai kyoki ek function ko sabhi object use kar sakte hai    This is a shared memory 
+
 
     Sketch.prototype.speak = function(){
 console.log("hello");
@@ -23,7 +23,6 @@ console.log("hello");
 let sketch1 =  new Sketch();
 
 
-// this : ek special keyword jiski value badal jati hia   likhte waqt iski value tay nhai hoti jab ye chalega tab tai hogi
 
 console.log(this);
 
@@ -32,10 +31,6 @@ function abcd(){
 }
 abcd();
 
-// Object ke ander function banane ko method kahte hai
-
-
-// methid me function banayege to this call karege to window aayega agar isi ko fet aarow function banyege to 
 
 let make = {
     name:"chetna",
@@ -51,19 +46,13 @@ make.fnc();
 
 
 
-//  yaad kese rakhe
 
-
-// 1 Global me value rahegi this ki  = window
-
-
-// 2  function me this = window
   function bd(){
     console.log(this);
   }
 bd();
 
-// 3   es5 function inside object -> object
+
 let vh ={
     name:"chetna",
     es5:function(){
@@ -74,7 +63,7 @@ vh.es5();
 
 
 
-// 4   es6 arrow function inside object -> window
+
 let hdd = {
     es6:()=>{
         console.log(this);
@@ -85,7 +74,7 @@ hdd.es6();
 
 
 
-// 5 es5 function inside es5 function inside object -> window
+
       let gdvd = {
         bb:function(){
             function vdg(){
@@ -98,7 +87,6 @@ hdd.es6();
 
 
 
-// 6 es6 function inside es5 function inside object -> object
 
 
  let vd = {
@@ -116,12 +104,7 @@ hdd.es6();
 
 
 
-    //   Call , Apply And bind
 
-// ek function mein this ki value window hoti hai, agar aap chaahte ho ki ######    [wo value window naa ho par koi aur objects ho] tab aap use kr sakte hoo call apply bine ka
-
-
-//  call   : ye function chalata hai and this value set karta hai
 
 let cal = {
     name:"chetna",
@@ -132,7 +115,7 @@ function bdbd(){
 }
 bdbd.call(cal);
 
-// apply  :  wahi karta hai jo call karta hai and arguments mein pahli value this ki and dosri value array me
+
 
 
 let aap = {
@@ -146,7 +129,7 @@ ba.apply(aap,[12,13,20]);
 
 
 
-// bind  : vahi karta hai jo call karta hai ye aapko naya function deta hai
+
 
 let bi = {
     name:"chetna",
@@ -156,6 +139,6 @@ function bn(a,b,c){
     console.log(this,a,b,c);
 }
 let result = bn.bind(aap,10,20,30);
-// bind call ki tarah hota hai ye naya function deta hai
+
 
 result();
